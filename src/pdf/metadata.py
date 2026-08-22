276dd5a1a07b3aacd683d5fe00f942b4147285dc
richie <richiepagard@gmail.com>
2026-08-22 00:47:00 -0400

    feat; Update Metadata Class and Its Module.
    - Implment a new method to format the creation date of the PDF Document.
    
    - Implement a new method to add / modify the PDF Document metadata (not completed yet).

--- src/pdf/metadata.py
+++ src/pdf/metadata.py
@@ -6,8 +6,9 @@ also processing the file with validations and handling the exceptions.
 
 import logging
 import pprint
+from datetime import date
 
-from pypdf import PdfReader
+from pypdf import PdfReader, PdfWriter
 from pypdf.errors import PyPdfError
 
 from src.utils import base_logger
@@ -38,6 +39,7 @@ class Metadata:
 
         self.file_path = file_path
         self.reader = PdfReader(file_path)
+        self.writer = PdfWriter()
 
     def file_metadata(self):
         """
@@ -67,6 +69,64 @@ class Metadata:
 
         return data
 
+    def add_metadata(
+        self,
+        author: str = None,
+        producer: str = None,
+        title: str = None,
+        subject: str = None,
+        creator: str = None
+    ):
+        """
+        Adding metadata to the file if it does not contain any metadata.
+        It only checks whether the file contains metadata or not, and
+        if not, it adds new metadata to it. In other words, it modify
+        the file metadata by adding some to it.
+        """
+        final_data = {}
+
+        _basic_metadata = {
+            "/Author": author,
+            "/Producer": producer,
+            "/Title": title,
+            "/Subject": subject,
+        }
+        _might_default = {
+            "/Creator": self.reader.metadata.creator or creator,
+            "/Producer": self.reader.metadata.producer or producer
+        }
+        final_data = {**_basic_metadata, **_might_default}
+
+        # Override the document's metadata by the 'final_data'
+        self.writer.metadata = {}
+        logger.debug(self.writer.metadata)
+
+        self.writer.metadata = final_data
+        self.writer.write(self.file_path)
+
+    def _creation_date_format(self, creation_date: str) -> str:
+        """
+        Helper function to format the creation date metadata
+        of the PDF document.
+        If the document has a Creation Date, the formatted date keeps
+        in the current date of document. But if does not contain any,
+        the formatted date set to the sent date from the client which provided
+        by the function arguments.
+
+        Arguments:
+            creation_date (str): The creation date client sent to set as document's metadata.
+                                It's format: "<year>-<month>-<day>".
+        """
+        creation_date = str(creation_date)
+        _formatted_date = None
+
+        if self.reader.metadata.creation_date:
+            _formatted_date = self.reader.metadata.creation_date
+        else:
+            _formatted_date = date.fromisoformat(creation_date)
+
+        return _formatted_date
+
     def __str__(self) -> str:
         """
         Format the output string of an object. Returns the retuned data
@@ -79,5 +139,11 @@ class Metadata:
         return result.replace("{", "{\n ", 1).rsplit("}", 1)[0] + "\n}"
 
 
-meta = Metadata("examples/college_management_system.pdf")
+meta = Metadata("examples/forfun.pdf")
+meta.add_metadata(
+    author="Dennis",
+    title="Just For Fun",
+    subject="Just For Fun...",
+)
+
 print(str(meta))
