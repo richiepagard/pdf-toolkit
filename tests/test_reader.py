2a3189866c03a075a91c62f49b5e09ecacf12aae
richie <richiepagard@gmail.com>
2026-08-29 23:59:32 -0400

    test; Update Reader Test Cases.
    - Wrote a new test case to test if the PDF file is a valid file to ensure reading the content and return them properly.
    
    - Wrote a new test case for Reader module to tests if the file is even exists or not to read it.

---
+++ tests/test_reader.py
@@ -0,0 +1,44 @@
+"""
+Tests the PDF Reader module's Reader class.
+
+- Tests valid PDF to ensure content is returned.
+- Tests multiple pages to ensure all pages are read.
+- Checks invalid or nonexistent path for file not found exceptions.
+- Tests the corrupted PDF exception from PdfReader exception.
+- Tests that the PdfError raised appropriately.
+"""
+
+import unittest
+from tempfile import NamedTemporaryFile
+
+from pypdf.errors import PdfReadError
+
+from src.pdf.reader import Reader
+
+
+class TestReader(unittest.TestCase):
+
+	def test_file_does_not_exist(self):
+		"""
+		Tests if the file trying to read is exists or not.
+		"""
+		fake_path = "this_file_does_not_exist.pdf"
+
+		with self.assertRaises(FileNotFoundError):
+			Reader(fake_path)
+
+	def test_invalid_pdf_file(self):
+		"""
+		Tests the PDF document validation to ensure
+		the content is returned properly.
+		"""
+		with NamedTemporaryFile(suffix=".pdf") as file:
+			file.write(b"This is not a real PDF!")
+			file.flush()
+
+			with self.assertRaises(PdfReadError):
+				Reader(file.name)
+
+
+if __name__ == "__main__":
+	unittest.main()
